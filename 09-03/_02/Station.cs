ed79717df14fa391db8dfa37841b6cf4ee5eebba
Vokivon <vokivon.p@gmail.com>
2019-02-04 18:02:18 +0300

    Добавил набросок класса Station<T>

---
+++ 09-03/_02/Station.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehiclesApp
+{
+    /*
+     * Параметризованный класс станции 
+     */
+    class Station<T>
+    {
+        public string Name { get; set; }
+        public int OnStation { get => _OnStationV.Count; }
+            
+        private List<T> _OnStationV { get; }
+
+        void InStation(T v)
+        {
+        }
+
+        T OutStation()
+        {
+            return default(T);
+        }
+
+        public Station()
+        {
+            _OnStationV = new List<T>();
+        }
+    }
+}
