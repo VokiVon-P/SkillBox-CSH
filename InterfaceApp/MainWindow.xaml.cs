ea991ea4d8a398c436289962761953a2947bae23
Vokivon <vokivon.p@gmail.com>
2019-05-12 18:57:00 +0300

    Закончил предварительный набросок интерфейса

--- InterfaceApp/MainWindow.xaml.cs
+++ InterfaceApp/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace InterfaceApp
         {
             InitializeComponent();
         }
+
+        
     }
 }
---
+++ InterfaceApp/TempCollections.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceApp
+{
+    public class SideBarHashTags : ObservableCollection<string>
+    {
+        public SideBarHashTags()
+        {
+            Add("#Цой Жив");
+            Add("#Ямакаси");
+            Add("Квантовый скачек");
+            Add("#кинопоиск");
+            Add("#рубль");
+            Add("#селфи");
+            Add("#ужин");
+            Add("#игры");
+        }
+    }
+}
